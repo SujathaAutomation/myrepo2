98a20ef51712d733dbed49c9f515639b51d331da
SujathaAutomation <Marysujatha11@gmail.com>
2022-05-18 14:10:41 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,9 @@
-
+/* Author of sample Sujatha k*/
+Class HelloWorld
+{
+  public staic void main(String args[])
+  {
+    system.out.println("HELLO WORLD ---  Sujatha k "); 
+    system.out.println(" HELLO WORLD --- Frisco");
+  }
+}
